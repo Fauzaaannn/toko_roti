b8cddcbb661a3d120ee7cfd7125f78b8015a6c7b
Fauzaaannn <fauzanabderrasheed@gmail.com>
2025-06-14 10:08:09 +0700

    merge ragil

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
-// import 'package:flutter/foundation.dart';
-// import 'package:device_preview/device_preview.dart';
-
 import 'package:flutter/material.dart';
 import 'package:toko_roti/view/auth/login.dart';
 import 'package:toko_roti/view/auth/register.dart';
 
+void main() {
   runApp(const MyApp());
-  // DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(title: 'Flutter Demo', home: const Login());
